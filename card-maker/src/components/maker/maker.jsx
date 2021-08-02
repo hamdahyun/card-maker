d7d4f0ed3102f722956302cd8c8e1d4422d244eb
dahyun Ham <dahuyn1@gmail.com>
2021-08-02 13:05:44 +0900

    add card repository

--- card-maker/src/components/maker/maker.jsx
+++ card-maker/src/components/maker/maker.jsx
@@ -1,96 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import styles from './maker.module.css'
-import Header from '../header/header';
-import Footer from '../footer/footer';
+import React, { useEffect , useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import styles from './maker.module.css';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
-const Maker = ({ FileInput, authService }) => {
-    const [cards, setCards] = useState({
-        '1': {
-            id: '1',
-            name: 'dahyun',
-            company: 'SSG',
-            theme: 'dark',
-            title: 'UI Developer',
-            email: 'dahuyn1@gmail.com',
-            message: 'go for it',
-            fileName: 'dahyun',
-            fileURL: null,
-        },
-        '2': {
-            id: '2',
-            name: 'dahyun2',
-            company: 'SSG',
-            theme: 'colorful',
-            title: 'UI Developer',
-            email: 'dahuyn1@gmail.com',
-            message: 'go for it',
-            fileName: 'dahyun',
-            fileURL: null,
-        },
-        '3': {
-            id: '3',
-            name: 'dahyun3',
-            company: 'SSG',
-            theme: 'light',
-            title: 'UI Developer',
-            email: 'dahuyn1@gmail.com',
-            message: 'go for it',
-            fileName: 'dahyun',
-            fileURL: null,
-        }
-    });
+const Maker = ({ FileInput, authService, cardRepository }) => {
+   const historyState = useHistory().state
+   const [cards, setCards] = useState({})
+   const [userId, setUserId] = useState(historyState && historyState.id);
 
-    const history = useHistory();
-    const onLogout = () => {
-        authService.logout();
-    } 
-    useEffect(() => {
-        authService.onAuthChange(user => {
-            if(!user) {
-                history.push('/');
-            }
+   const history = useHistory()
+   const onLogout = () => {
+      authService.logout()
+   }
+
+   useEffect(() => {
+      authService.onAuthChange(user => {
+         if (user) {
+            setUserId(user.uid)
+         } else {
+            history.push(`/`)
+
+         }
+      })
+   });
+
+   const createOrupdateCard = card => {
+      setCards(cards => {
+         const updated = {...cards};
+         updated[card.id] = card;
+         return updated;
         })
-   })
-
-   const createOrUpdateCard = (card) => {
-        // 성능 진짜 안좋아.
-        // const updated = cards.map(item => {
-        //     if(card.id === item.id) {
-        //         return m
-        //     }
-        //     return item;
-        // })
-
-        // 키로 만들어서 성능을 향상시켜
-        setCards(cards => {
-            const updated = {...cards};
-            updated[card.id] = card;
-            return updated;
-        });
+    
+      cardRepository.saveCard(userId, card)
    }
 
+   const deleteCard = card => {
+      setCards(cards => {
+         const updated = {...cards};
+         delete updated[card.id];
+         return updated;
+      })
 
-   const deleteCard = (card) => {
-    setCards(cards => {
-        const updated = {...cards};
-        delete updated[card.id];
-        return updated;
-    });
+      cardRepository.removeCard(userId, card)
    }
 
-    return (
-        <section className={styles.maker}>
-            <Header onLogout={onLogout} />
-            <div className={styles.container}>
-                <Editor FileInput={FileInput} cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard}/>
-                <Preview cards={cards} />
-            </div>
-            <Footer />
-        </section>
-    )
-}
+   return (
+      <section className={styles.maker}>
+         <Header onLogout = {onLogout}/>
+         <div className={styles.container}>
+            <Editor  FileInput= {FileInput} cards = {cards} addCard = {createOrupdateCard} updateCard = {createOrupdateCard} deleteCard = {deleteCard}/>
+            <Preview cards = {cards}/>
+         </div>
+         <Footer />
+      </section>
+   )
+};
 
 export default Maker;
